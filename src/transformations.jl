6e6081ec83841f7d972dbd7526d68d5f78795da5
Yuri <50186716+Yuricst@users.noreply.github.com>
2022-10-18 18:14:12 -0400

    Update transformations.jl

--- src/transformations.jl
+++ src/transformations.jl
@@ -3,9 +3,41 @@ Transformations
 """
 
 
-function transform_SunB1_to_EMrot(state, )
+"""
+Transform state from Earth-Moon rotating frame to Sun-B1 rotating frame.
+Careful with sign of ωs!! (should be negative)
+"""
+function transform_EMrot_to_SunB1(
+    state,
+    θs::Real,
+    ωs::Real,
+)
+    ωm = -ωs
+    θm = π - θs
+    cos_θm = cos(θm)
+    sin_θm = sin(θm)
+    C = [
+        cos_θm -sin_θm 0
+        sin_θm  cos_θm 0
+        0       0      1
+    ]
+    Cdot = [
+        -ωm*sin_θm -ωm*cos_θm 0
+         ωm*cos_θm -ωm*sin_θm 0
+         0          0         1
+    ]
+    state_conv = vcat(
+        C*state[1:3],
+        C*state[4:6] + Cdot*state[1:3],
+    )
+    return state_conv
 end
 
 
-function transform_EMrot_to_SunB1(state, )
+
+"""
+Transform state from Earth-Moon rotating frame to Sun-B1 rotating frame.
+Careful with sign of ωm!! (should be positive)
+"""
+function transform_SunB1_to_EMrot(state, )
 end
