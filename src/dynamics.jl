810fbcb3c83412b4ae4d352de8dc55bd26ef3411
UzTak <49874360+UzTak@users.noreply.github.com>
2022-10-19 09:56:44 -0400

    change the notations

--- src/dynamics.jl
+++ src/dynamics.jl
@@ -41,10 +41,10 @@ Right-hand side expression for state-vector in BCR4BP
 # Arguments
     - `du`: cache array of duative of state-vector, mutated
     - `u`: state-vector
-    - `p`: parameters, where p = [μ1, μ2, as, θ0, ωM, τ, β, γ]
+    - `p`: parameters, where p = [μ2, μS, as, θ0, ωM, τ, γ, β]
         m* : mE + mL (6.0455 * 10^22)
-        μ1 : mL / m* (0.012150585609624)
-        μ2 : mS / m* (0.00000303951)
+        μ2 : mL / m* (0.012150585609624)
+        μS : mS / m* (0.00000303951)
         as : (sun-B1 distance) / l* (388.709677419)
             l* : Earth-moon distance (384,400 km)
         ωM : Earth-Moon line's angular velocity around E-M barycenter
@@ -57,7 +57,7 @@ function rhs_bcr4bp!(du,u,p,t)
     # unpack state
     x, y, z = u[1], u[2], u[3]
     vx, vy, vz = u[4], u[5], u[6]
-    μ1, μ2, as, θ0, ωM = p[1], p[2], p[3], p[4], p[5]
+    μ2, μS, as, θ0, ωM = p[1], p[2], p[3], p[4], p[5]
     τ, γ, β = p[6], p[7], p[8]
 
     θ = θ0 + ωM * t
@@ -68,8 +68,8 @@ function rhs_bcr4bp!(du,u,p,t)
 
     # compute distances
     r30 = sqrt((as - x)^2 + y^2 + z^2)
-    r31 = sqrt((-μ1*cos(θ) - x)^2 + (-μ1*sin(θ) - y)^2 + z^2)
-    r32 = sqrt(((1-μ1)*cos(θ) - x)^2 + ((1-μ1)*sin(θ) - y)^2 + z^2)
+    r31 = sqrt((-μ2*cos(θ) - x)^2 + (-μ2*sin(θ) - y)^2 + z^2)
+    r32 = sqrt(((1-μ2)*cos(θ) - x)^2 + ((1-μ2)*sin(θ) - y)^2 + z^2)
 
     # derivatives of positions
     du[1] = u[4]
@@ -77,9 +77,9 @@ function rhs_bcr4bp!(du,u,p,t)
     du[3] = u[6]
     
     # forces applied (Sun, Earth, Moon, and thrust term)
-    Fx = μ2 / r30^3 * (-as - x) + (1-μ1) / r31^3 * (-μ1*cos(θ) - x)    + μ1 / r32^3 * ((1-μ1)*cos(θ) - x) + Tx
-    Fy = μ2 / r30^3 * (-y)      + (1-μ1) / r31^3 * (-μ1*sin(θ) - y)    + μ1 / r32^3 * ((1-μ1)*sin(θ) - y) + Ty
-    Fz = μ2 / r30^3 * (-z)      + (1-μ1) / r31^3 * (-z)                 + μ1 / r32^3 * (-z)               + Tz
+    Fx = μS / r30^3 * (-as - x) + (1-μ2) / r31^3 * (-μ2*cos(θ) - x)    + μ2 / r32^3 * ((1-μ2)*cos(θ) - x) + Tx
+    Fy = μS / r30^3 * (-y)      + (1-μ2) / r31^3 * (-μ2*sin(θ) - y)    + μ2 / r32^3 * ((1-μ2)*sin(θ) - y) + Ty
+    Fz = μS / r30^3 * (-z)      + (1-μ2) / r31^3 * (-z)                 + μ2 / r32^3 * (-z)               + Tz
 
     du[4] = 2*vy  + x + Fx
     du[5] = -2*vx + y + Fy
