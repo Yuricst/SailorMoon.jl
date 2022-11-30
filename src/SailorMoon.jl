10b3840b6bc5b0ccb5f39138bd3fdc20a07592d6
UzTak <49874360+UzTak@users.noreply.github.com>
2022-11-30 17:01:06 -0500

    update

--- src/SailorMoon.jl
+++ src/SailorMoon.jl
@@ -8,6 +8,7 @@ import ForwardDiff
 import DiffResults
 import FiniteDiff
 
+include("lpo/build_lpo.jl")
 include("integrator.jl")
 include("deltaV_transcription.jl")
 include("dynamics.jl")
@@ -15,6 +16,7 @@ include("transformations.jl")
 include("initialize_ode.jl")
 include("propagator.jl")
 include("integrator.jl")
+include("multiple-shooting.jl")
 include("fitness.jl")
 
 
