bb0330876d75cd262ed1080b9b0a23559ef0a3b2
UzTak <yujitakubo@gmail.com>
2023-06-10 14:14:27 -0400

    up

--- src/deltaV_transcription.jl
+++ src/deltaV_transcription.jl
@@ -129,6 +129,48 @@ function dv_EMrotdir_sb1frame(μS::Float64, as::Float64, θ::Float64, state0::Ve
 end
 
 
+"""
+    Earth-Moon rotating direction 
+    According to Scheuerle et al. (2023 AAS), thrusting in the Earth-Moon rotating direction (i.e., tangential direction to the B1->SC vector)
+    is the optimal in order to enhance the Earth-Moon instantaneous Jacobi constant. 
+    Here, S-B1 frame refers to the reduced S-B1 frame where B2 = Sun 
+"""
+function dv_antiEMrotdir_sb1frame(μS::Float64, as::Float64, θ::Float64, state0::Vector{Float64}, p::Vector{Float64})
+    τ, γ, β = p[1], p[2], p[3]
+
+    # vector B1 -> SC 
+    x = state0[1] - as
+    y = state0[2]
+
+    dir = [
+        y/sqrt(x^2+y^2) 
+        -x/sqrt(x^2+y^2)
+        0
+        ]
+    
+    sin_β = sin(β)
+    cos_β = cos(β)
+    sin_γ = sin(γ)
+    cos_γ = cos(γ)
+    
+    # rot about y-axis 
+    rot1 = [
+        cos_β  0 sin_β
+        0      1 0
+        -sin_β 0 cos_β
+    ]
+
+    # rot about z-axis 
+    rot2 = [
+        cos_γ -sin_γ 0 
+        sin_γ cos_γ  0
+        0     0      1
+    ]
+
+    return τ * rot1 * rot2 * dir 
+
+end
+
 
 
 
