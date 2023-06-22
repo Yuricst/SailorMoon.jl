c12aaa5d39d0dda8d7362230b320bf55694663a6
UzTak <49874360+UzTak@users.noreply.github.com>
2023-06-22 13:23:28 -0700

    update

--- src/plot_func.jl
+++ src/plot_func.jl
@@ -11,3 +11,17 @@ function plot_circle(radius, x, y, m=50)
     end
     return circle
 end
+
+
+# compute JC from state in EMrot
+function compute_JC(state::Vector)
+    x, y, z = state[1], state[2], state[3]
+    vx, vy, vz = state[4], state[5], state[6]
+    r13 = norm([x-param3b.mu2, y, z])  # Earth - SC
+    r23 = norm([x-param3b.mu1, y, z])  # Moon - SC
+    
+    U = 1/2*(x^2+y^2) + param3b.mu1 / r13 + param3b.mu2 / r23 
+    JC = 2*U - (vx^2 + vy^2 + vz^2)
+    
+    return JC 
+end
